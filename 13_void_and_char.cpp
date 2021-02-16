8e2d03fa233949cf76df62fa1c42e3c16fd931e7
kleimanmarek@gmail.com <kleimanmarek@gmail.com>
2021-02-16 09:42:33 +0100

    upgrade on my last project

--- 13_void_and_char.cpp
+++ 13_void_and_char.cpp
@@ -34,24 +34,36 @@ void max_pola(int ary[12],int n)
     vypis_pole(ary, n);
 }
 
+void avridge(int ary[12],int n)
+{
+    int avg=0;
+    for (int i=0; i<n; i++)
+    {
+        avg += ary[i];
+    }
+    std::cout << "Priemer je " << double (avg/n) << std::endl;
+}
+
 int main()
 {
     char menu;
     bool end=true;
-//    int n=12;
-    int cis[12];
+    int ea=12;
+    int cis[ea];
     while (end)
     {
         std::cout << "Zvol si jedno: v - vypis pola" << std::endl;
         std::cout << "               z - zadaj pole" << std::endl;
         std::cout << "               m - maximum pola" << std::endl;
+        std::cout << "               a - priemer pola" << std::endl;
         std::cout << "               ESC - koniec" << std::endl;
         menu = getch();
         switch (menu)
         {
-            case 'v': vypis_pole(cis, 12); break;
-            case 'z': nahodne_pole(cis, 12); break;
-            case 'm': max_pola(cis, 12); break;
+            case 'a': avridge(cis, ea); break;
+            case 'v': vypis_pole(cis, ea); break;
+            case 'z': nahodne_pole(cis, ea); break;
+            case 'm': max_pola(cis, ea); break;
             case 27: end = false; break;
             default:;
         }
